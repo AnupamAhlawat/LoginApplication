db8b0911d36d81cfce56d659e2931d986a885e4d
Anupam Ahlawat <anupam.1010@gmail.com>
2015-03-31 17:56:36 +1100

    Committing the Login Application File.

--- LoginApplication/src/batchLaunchApplication/LaunchBrowser.java
+++ LoginApplication/src/batchLaunchApplication/LaunchBrowser.java
@@ -8,7 +8,6 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -19,10 +18,11 @@ public class LaunchBrowser {
 	public String usrName;
 	public String pwd;
 
-	public static void main(String[] args) throws InvalidFormatException, IOException, NullPointerException {
+	public static void main(String[] args) throws InvalidFormatException, IOException, NullPointerException, InterruptedException {
 		
 		//CODE TO READ VALUES FROM EXCEL FILE
 		//FileInputStream inputFile = new FileInputStream("..\\Resources\\JavaReadValues.xlsx");
+		System.out.println("Initializing File");
 		FileInputStream inputFile = new FileInputStream("C:\\Users\\anupam.ahlawat\\git\\LoginApplication\\LoginApplication\\Resources\\JavaReadValues.xlsx");
 		Workbook wb = WorkbookFactory.create(inputFile);
 		Sheet sheet = wb.getSheet("LoginInformation");
@@ -30,24 +30,28 @@ public class LaunchBrowser {
 		ReadExcel readExcel = new ReadExcel();
 		
 		//INITIALIZING IE WEBDRIVER
-		System.setProperty("webdriver.ie.driver", "C:\\Users\\anupam.ahlawat\\git\\LoginApplication\\LoginApplication\\Resources\\IEDriverServer.exe");
-		
+		System.out.println("Initializing Selenium Java Driver");
+		System.setProperty("webdriver.ie.driver", "C:\\Users\\anupam.ahlawat\\git\\LoginApplication\\LoginApplication\\Resources\\IEDriverServer_x64_2.45.0\\IEDriverServer.exe");
 		for( int i=0; i<MAX_ROW_COUNT; i++){
 			String link = readExcel.getLink(sheet, ROW_COUNT);
 			String usrName = readExcel.getUsername(sheet, ROW_COUNT);
 			String pwd = readExcel.getPassword(sheet, ROW_COUNT);
 			System.out.println("The link is :" + link + "\n" +"The username is:" + usrName + "\n" +"The password is: " + pwd);
-			
 			//STARTING NEW INSTANCE OF IE
 			InternetExplorerDriver iedriver = new InternetExplorerDriver();
 			iedriver.get(link);
-			WebElement username = iedriver.findElement(By.id("idDiv_PWD_UsernameExample"));
+			//Wait for jvm to come up
+			Thread.sleep(4000);
+			WebElement username = iedriver.findElement(By.name("j_username"));
 			username.sendKeys(usrName);
-			WebElement password = iedriver.findElement(By.id("idDiv_PWD_PasswordExample"));
+			WebElement password = iedriver.findElement(By.name("j_password"));
 			password.sendKeys(pwd);
-			WebElement loginButton = iedriver.findElement(By.id("idSIButton9"));
+			//WebElement loginButton = iedriver.findElement(By.value("idSIButton9"));
+			//loginButton.click();
+			WebElement loginButton = iedriver.findElementByXPath("//*[@type='submit']");
 			loginButton.click();
 			ROW_COUNT++;
+			
 			
 		}
 		
